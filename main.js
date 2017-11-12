c3a7517221170a161a7b19eb103b5b885792141e
NicholasMacedo <nicholas.francis.macedo@gmail.com>
2017-11-12 15:12:18 -0500

    More Vue

--- main.js
+++ main.js
@@ -21,3 +21,44 @@ var demoThree = new Vue({
     seen: true
   }
 })
+
+// Vue Demo Four: Display Iterating over an Array of Items
+var demoFour = new Vue({
+  el: '#demo4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+
+// Vue Demo Five: Do something on user actions.
+var demoFive = new Vue({
+  el: '#demo5',
+  data: {
+    message: 'This is a string!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+// Vue Demo Six: Bind fields to display dynamically.
+var demoSix = new Vue({
+  el: '#demo6',
+  data: {
+    message: 'Testing!'
+  }
+})
+
+// Vue Demo Six: Bind fields to display dynamically.
+var demoSeven = new Vue({
+  el: '#demo7',
+  data: {
+    rawHtml: '<p> This is inside an injected P tag</p>'
+  }
+})
